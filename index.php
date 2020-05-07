d18c82e49bdca828d6d51668decdb283acdf0d70
hyoketsu <4650065@gmail.com>
2020-05-07 18:27:48 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,2 @@
 <?php
-    $url = [
-        "https://headlines.yahoo.co.jp/rss/all-dom.xml",
-        "https://headlines.yahoo.co.jp/rss/all-c_int.xml",
-        "https://headlines.yahoo.co.jp/rss/all-bus.xml",
-        "https://headlines.yahoo.co.jp/rss/all-c_ent.xml",
-        "https://headlines.yahoo.co.jp/rss/all-c_spo.xml",
-        "https://headlines.yahoo.co.jp/rss/all-c_sci.xml",
-        "https://headlines.yahoo.co.jp/rss/all-c_life.xml",
-        "https://headlines.yahoo.co.jp/rss/all-loc.xml",
-        "https://news.yahoo.co.jp/polls/rss.xml",
-        "https://news.yahoo.co.jp/byline/rss/all.xml",
-        "https://b.hatena.ne.jp/hotentry.rss"
-    ];
-    $xml = file_get_contents("https://b.hatena.ne.jp/hotentry.rss");
-    header("Content-type: application/xml; charset=UTF-8");
-    print $xml;
-?>
+    if(isset($_GET["url"])) echo file_get_contents($_GET["url"]);
